510d7b07420834b7d64b5307ba32feb3a9edcd7e
yuvraaj <yuvraaj@028e6f87-2093-faaf-8fb2-0843b45e221b>
2011-04-23 18:47:09 +0000

    merge branch to trunk, cleanup branches

--- branches/retest/src/ChildWindowBase.cpp
+++
@@ -1,9 +0,0 @@
-#include "ChildWindowBase.h"
-#include "Singletons.h"
-
-ChildWindowBase::ChildWindowBase(QWidget *parent, Qt::WindowFlags  f)
-: ChildWindowBaseClass(parent, f)
-{
-    OsDependent &osd = Singletons::getRef().getOSD ();
-    osd.setDefaultWindowAttributes (this);
-}//ChildWindowBase::ChildWindowBase
