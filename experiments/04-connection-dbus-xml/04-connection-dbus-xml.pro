bf244b2eddd6ef156ebeb05ddabb51a379a7635d
yuvraaj@gmail.com <yuvraaj@gmail.com@028e6f87-2093-faaf-8fb2-0843b45e221b>
2012-10-24 01:15:32 +0000

    theres no point adding client side interfaces

--- experiments/04-connection-dbus-xml/04-connection-dbus-xml.pro
+++ experiments/04-connection-dbus-xml/04-connection-dbus-xml.pro
@@ -7,17 +7,21 @@ QT += dbus
 
 # Input
 HEADERS += cm_adapter.h             \
-           cm_proxy.h               \
            connection_adapter.h     \
-           connection_proxy.h       \
            protocol_adapter.h       \
-           protocol_proxy.h         \
            shared_data_types.h
 
 SOURCES += cm_adapter.cpp           \
-           cm_proxy.cpp             \
            connection_adapter.cpp   \
-           connection_proxy.cpp     \
            main.cpp                 \
            protocol_adapter.cpp     \
+
+exists(not-really) {
+HEADERS += cm_proxy.h               \
+           connection_proxy.h       \
+           protocol_proxy.h
+
+SOURCES += cm_proxy.cpp             \
+           connection_proxy.cpp     \
            protocol_proxy.cpp
+}
--- experiments/04-connection-dbus-xml/create_ifaces.sh
+++ experiments/04-connection-dbus-xml/create_ifaces.sh
@@ -2,10 +2,13 @@
 
 rm -f cm_* connection_* protocol_*
 
-qdbusxml2cpp -v -a cm_adapter -p cm_proxy -v -i shared_data_types.h cm.xml
+#qdbusxml2cpp -v -a cm_adapter -p cm_proxy -v -i shared_data_types.h cm.xml
+qdbusxml2cpp -v -a cm_adapter -v -i shared_data_types.h cm.xml
 echo cm.xml done.
-qdbusxml2cpp -v -a connection_adapter -p connection_proxy -v -i shared_data_types.h connection.xml
+#qdbusxml2cpp -v -a connection_adapter -p connection_proxy -v -i shared_data_types.h connection.xml
+qdbusxml2cpp -v -a connection_adapter -v -i shared_data_types.h connection.xml
 echo connection.xml done.
-qdbusxml2cpp -v -a protocol_adapter -p protocol_proxy -v -i shared_data_types.h protocol.xml
+#qdbusxml2cpp -v -a protocol_adapter -p protocol_proxy -v -i shared_data_types.h protocol.xml
+qdbusxml2cpp -v -a protocol_adapter -v -i shared_data_types.h protocol.xml
 echo protocol.xml done.
 
