baa24bbcfe7995aee5cc7d1e1cfc9675451eb16f
yuvraaj <yuvraaj@028e6f87-2093-faaf-8fb2-0843b45e221b>
2012-10-24 07:21:33 +0000

    fill up the cm code... lots more to be done

--- experiments/04-connection-dbus-xml/QGVConnectionManager.cpp
+++ experiments/04-connection-dbus-xml/QGVConnectionManager.cpp
@@ -1,5 +1,11 @@
 #include "QGVConnectionManager.h"
 
+#define Conn_Mgr_Param_Flags_Required       1
+#define Conn_Mgr_Param_Flags_Register       2
+#define Conn_Mgr_Param_Flags_Has_Default    4
+#define Conn_Mgr_Param_Flags_Secret         8
+#define Conn_Mgr_Param_Flags_DBus_Property 16
+
 QGVConnectionManager::QGVConnectionManager(QObject *parent)
 : QObject(parent)
 {
@@ -13,6 +19,21 @@ Qt_Type_a_susv
 QGVConnectionManager::GetParameters(const QString &Protocol)
 {
     Qt_Type_a_susv rv;
+    Struct_susv susv;
+    
+    susv.s1 = "user";
+    susv.u  = Conn_Mgr_Param_Flags_Required | Conn_Mgr_Param_Flags_Register;
+    susv.s2 = "String containing the DBus signature";
+    susv.v  = QString();
+    rv.append(susv);
+
+    susv.s1 = "pass";
+    susv.u  = Conn_Mgr_Param_Flags_Required | Conn_Mgr_Param_Flags_Register |
+              Conn_Mgr_Param_Flags_Secret;
+    susv.s2 = "String containing the DBus signature";
+    susv.v  = QString();
+    rv.append(susv);
+    
     return rv;
 }//QGVConnectionManager::GetParameters
 
@@ -20,6 +41,7 @@ QStringList
 QGVConnectionManager::ListProtocols()
 {
     QStringList rv;
+    rv.append("qgv");
     return rv;
 }//QGVConnectionManager::ListProtocols
 
@@ -29,5 +51,6 @@ QGVConnectionManager::RequestConnection(const QString &Protocol,
                                         QDBusObjectPath &Object_Path)
 {
     QString rv;
+    //TODO: Must emit NewConnection on success
     return rv;
 }//QGVConnectionManager::RequestConnection
